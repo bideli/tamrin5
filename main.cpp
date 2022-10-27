44c010c067f6711511f5664afeb49cb4de35f6ad
bideli <115703411+bideli@users.noreply.github.com>
2022-10-27 19:33:14 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    cout<<"\n*********"<<"    *********";
+    cout<<"\n**"<<"           **"<<"       *";
+    cout<<"\n**"<<"           **"<<"        *";
+    cout<<"\n**"<<"           **"<<"       *";
+    cout<<"\n*********"<<"    **********";
+    cout<<"\n**"<<"           **"<<"         *";
+    cout<<"\n**"<<"           **"<<"          *";
+    cout<<"\n**"<<"           **"<<"         *";
+    cout<<"\n**"<<"           ***********";
+    getch();
+    return 0;
+}
+
