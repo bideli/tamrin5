6e3acbfcbc27fae1cf808c69c4b4850bbee11e43
bideli <115703411+bideli@users.noreply.github.com>
2022-11-02 20:01:10 +0330

    Add files via upload

---
+++ main tamrin5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+    cout<<"\n This is the first letter of my first name:";
+    cout<<"\n\n\n ************";
+    cout<<"\n **";
+    cout<<"\n **";
+    cout<<"\n **";
+    cout<<"\n ************";
+    cout<<"\n **";
+    cout<<"\n **";
+    cout<<"\n **";
+    cout<<"\n **";
+    getch();
+    return 0;
+}
+
